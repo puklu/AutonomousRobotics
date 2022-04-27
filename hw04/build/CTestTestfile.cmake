0360680851080b2b65186dffb36dd3d6b07f2fed
Vivek <52951152+VivBat@users.noreply.github.com>
2022-04-27 23:26:12 +0200

    Delete hw04 directory

--- hw04/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vivek/Documents/ROS/hw04/src
-# Build directory: /home/vivek/Documents/ROS/hw04/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("aro_frontier/aro_frontier")
-subdirs("aro_frontier/aro_msgs")
--- hw04/devel/share/common-lisp/ros/aro_msgs/msg/_package_FollowPathResult.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package aro_msgs-msg)
-(cl:export '(FINALPOSITION-VAL
-          FINALPOSITION
-))
