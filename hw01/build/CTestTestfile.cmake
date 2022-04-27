d29fa769ceeda38557d2acfcadfd4c6e04631e2f
Vivek <52951152+VivBat@users.noreply.github.com>
2022-04-27 23:04:18 +0200

    Delete hw01 directory

--- hw01/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vivek/Documents/ROS/hw01/src
-# Build directory: /home/vivek/Documents/ROS/hw01/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("aro_scan")
