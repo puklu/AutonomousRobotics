098634041815218d3230cfdfeab415d73ac59de9
Vivek <52951152+VivBat@users.noreply.github.com>
2022-04-28 19:01:08 +0200

    Delete hw02 directory

--- hw02/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vivek/Documents/ROS/hw02/src
-# Build directory: /home/vivek/Documents/ROS/hw02/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("aro_sim")
-subdirs("aro_robocop")
