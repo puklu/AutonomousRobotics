5a53955ec1cabbb4131287f6e066e0f06650fe3b
Vivek <52951152+VivBat@users.noreply.github.com>
2022-04-28 19:00:45 +0200

    Delete hw01 directory

--- hw01/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/vivek/Documents/ROS/hw01/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/vivek/Documents/ROS/hw01/devel;/opt/ros/noetic'.split(';') if '/home/vivek/Documents/ROS/hw01/devel;/opt/ros/noetic' != '' else []
