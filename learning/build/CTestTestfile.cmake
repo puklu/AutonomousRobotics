f3249c9dd05640c2add9143af7d00b4e1cd4eac6
Vivek <52951152+VivBat@users.noreply.github.com>
2022-04-28 19:02:41 +0200

    Delete learning directory

--- learning/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vivek/Documents/ROS/learning/src
-# Build directory: /home/vivek/Documents/ROS/learning/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("my_package1")
--- learning/build/my_package1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vivek/Documents/ROS/learning/src/my_package1
-# Build directory: /home/vivek/Documents/ROS/learning/build/my_package1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
